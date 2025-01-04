57dfd950252ae80f8e31383d2856ac8df95e74ad
Gülsüm Bulut <glsmblt13@hotmail.com>
2025-01-04 20:50:14 +0300

    v4

--- src/Runner.java
+++ src/Runner.java
@@ -5,6 +5,17 @@ public class Runner {
         System.out.println("Değişiklik 1");//ws
 
 
+        //versiyon 2
+        System.out.println("Değişiklik 2");
+
+        // git init komutu projenin başlangıcında kullanılır. Programı her açışımızda kullanmayız
+        // bir kere git init komutu yazmamız yeterli projenin başlangıcında unuttuysak
+        // ilerlerken yapılabilir ancak bu o zamana kadar yazılan kodların hepsini tek versiyon olarak
+        // geçer
+
+
+
+
 
     }
 }
